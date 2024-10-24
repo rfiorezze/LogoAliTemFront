0cfd1711ec91a2de155910b38a6af950d16580bb
Renato Fiorezze Silva <renato.fsilva@parceiro.mrv.com.br>
2024-10-24 00:12:18 -0300

    MVP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiURL: 'http://www.logoalitem.com.br:2030/'
+    apiURL: 'https://www.logoalitem.com.br:2030/'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: true,
-    apiURL: 'http://www.logoalitem.com.br:2030/'
+    apiURL: 'https://www.logoalitem.com.br:2030/'
   };
   
   /*
