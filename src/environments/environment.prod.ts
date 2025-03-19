8f26c82d7ba3dac82d3e96cf430ddb87e0e4d69e
Renato Fiorezze Silva <renato.fsilva@parceiro.mrv.com.br>
2025-03-19 19:22:05 -0300

    Inclusão calculo reboque

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    apiURL: 'https://api.logoalitem.com.br/',
-    googleMapsApiKey: 'AIzaSyDY73zLaEyr-sZpbRRPnJPiSiIV95ri2aw'
+    apiURL: 'https://api.logoalitem.com.br/'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: true,
-  apiURL: 'https://api.logoalitem.com.br/',
-  googleMapsApiKey: 'AIzaSyDY73zLaEyr-sZpbRRPnJPiSiIV95ri2aw'
+  apiURL: 'https://api.logoalitem.com.br/'
 };
   
   /*
